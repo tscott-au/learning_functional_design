f2913cf2c8f1bd7c671564d26352cc74eba8c6c6
tscott-au <tim@nepelle.com.au>
2017-10-11 10:36:57 +1100

    Initial commit

---
+++ lib/learning_functional_design.ex
@@ -0,0 +1,18 @@
+defmodule LearningFD do
+  @moduledoc """
+  Documentation for LearningFD.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> LearningFD.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/learning_functional_design_test.exs
@@ -0,0 +1,8 @@
+defmodule LearningFDTest do
+  use ExUnit.Case
+  doctest LearningFD
+
+  test "greets the world" do
+    assert LearningFD.hello() == :world
+  end
+end
